1e5f6b4416de1b63c800e3e17e442f64e68522a2
verenichvlad <verenichvlad@gmail.com>
2015-04-09 12:23:23 +0200

    Naprawa

--- src/AppBundle/Entity/Antybiotic.php
+++ src/AppBundle/Entity/Antybiotic.php
@@ -6,21 +6,32 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Antybiotic
+ *
+ * @ORM\Table()
+ * @ORM\Entity
  */
 class Antybiotic
 {
     /**
      * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
      */
     private $name;
 
     /**
      * @var string
+     *
+     * @ORM\Column(name="classification", type="string", length=255)
      */
     private $classification;
 
